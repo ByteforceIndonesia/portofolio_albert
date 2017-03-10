a79b24ee070fca546ecec50437cc9a997d2647d2
Jonathan Hosea <jonathan.hosea@me.com>
2017-03-10 13:52:26 +0700

    skeleton finished

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,160 @@
 <section id="wrapper">
-	
-	
-	
-	
+	<div class="container-fluid">
+		<section id="pageOne">
+		<div class="container">
+			<div class="row">
+				<!-- Left -->
+				<div class="col-lg-4">
+					<div class="headPicture">
+						<img src="<?php echo base_url() . 'assets/images/potrait.jpg' ?>" alt="">
+					</div>
+				</div>
+
+				<!-- Right -->
+				<div class="col-lg-8">
+					<div class="nameLine">
+						<h1>Albert<br>Putra<br>Purnama</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		</section>
+
+		<section id="pageTwo">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- Rectangle -->
+					<div class="col-lg-4">
+						<img src="<?php echo base_url() . 'assets/images/icons/carrer.png' ?>" alt="">
+					</div>
+
+					<!-- Carrer -->
+					<div class="col-lg-8">
+						<h1>Carrer</h1>
+					</div>
+				</div>
+				<!-- Year -->
+				<div class="col-lg-3">
+					<h3>2012</h3>
+				</div>
+				<!-- Line -->
+				<div class="col-lg-1">
+					<hr width="5" size="500">
+				</div>
+				<!-- History -->
+				<div class="col-lg-8">
+					<ul>
+						<li>
+							<h3>Student at SMAK 5 PENABUR</h3>
+						</li>
+						<li>
+							<div class="panelHistory">
+								<h3>CEO of Dazzle Corporation</h3>
+								<p>
+									Managed to import low budget sport equiptment to middle-low class people. Developed deep relationship with forwarding companies responsible for importing goods from China.
+								</p>
+							</div>
+						</li>
+						<li>
+							<h3>Diablo Valley College</h3>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		</section>
+
+		<section id="pageThree">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="col-lg-4">
+							<img src="<?php echo base_url() . 'assets/images/icons/ability.png' ?>" alt="">
+						</div>
+						<div class="col-lg-8">
+							<h1>ABILITY</h1>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<ul>
+							<li>
+								<h4>C++</h4>
+								<img src="<?php echo base_url() . 'assets/images/icons/ability.png' ?>" width="20px">
+								<!-- TO:DO BAR -->
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="pageFour">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="col-lg-4">
+							<img src="<?php echo base_url() . 'assets/images/icons/portfolio.png' ?>" alt="">
+						</div>
+						<div class="col-lg-8">
+							<h1>PORTFOLIO</h1>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+				<!-- TO:DO get from database and logic here -->
+					<div class="col-lg-12">
+						<div class="col-lg-3">
+							<div class="panel">
+								lalalalala
+							</div>
+						</div>
+
+						<div class="col-lg-3">
+							<div class="panel">
+								lalalalala
+							</div>
+						</div>
+
+						<div class="col-lg-3">
+							<div class="panel">
+								lalalalala
+							</div>
+						</div>
+
+						<div class="col-lg-3">
+							<div class="panel">
+								lalalalala
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="pageFive">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-5">
+						<div class="whiteFooter">
+							&nbsp
+						</div>
+					</div>
+					<div class="col-lg-7">
+						<div class="col-lg-6">
+							<img src="<?php echo base_url() . 'assets/images/logo.php' ?>" alt="">
+						</div>
+						<div class="col-lg-6">
+							<p>albertputrapurnama@gmail.com</p>
+							<p>+1 925 238 6500</p>
+							<p>instagram.com/albertputrapurnama</p>
+							<p>linkedin.com/in/albertputrapurnama</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </section>
--- application/views/templates/template_home.php
+++ application/views/templates/template_home.php
@@ -8,9 +8,9 @@
 	</title>
 	
 	<!-- CSS -->
-	<link rel="text/stylesheet" href="<?php echo base_url() . CSS_DIR . 'style.css' ?>">
-	<link rel="text/stylesheet" href="<?php echo base_url() . CSS_DIR . 'bootstrap.min.css' ?>">
-	<link rel="text/stylesheet" href="<?php echo base_url() . CSS_DIR . 'bootstrap-theme.min.css' ?>">
+	<link rel="stylesheet" href="<?php echo base_url() . CSS_DIR . 'style.css' ?>">
+	<link rel="stylesheet" href="<?php echo base_url() . CSS_DIR . 'bootstrap.min.css' ?>">
+	<link rel="stylesheet" href="<?php echo base_url() . CSS_DIR . 'bootstrap-theme.min.css' ?>">
 
 	<!-- JS -->
 	<script src="<?php echo base_url() . JS_DIR . 'jquery-3.1.1.min.js' ?>"></script>

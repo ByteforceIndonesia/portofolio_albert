6ad62804d4342ce76e0eb46cb0b59f63d20ea5f5
Jonathan Hosea <jonathan.hosea@me.com>
2017-05-22 14:37:56 +0700

    db backup

---
+++ dbbackup/app (1).sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 22, 2017 at 09:37 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ability`
+--
+
+CREATE TABLE `ability` (
+  `id` int(11) NOT NULL,
+  `language` varchar(255) NOT NULL,
+  `value` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ability`
+--
+
+INSERT INTO `ability` (`id`, `language`, `value`) VALUES
+(1, 'C++', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(20) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`, `hash`) VALUES
+(1, 'test', '$2y$10$k02xt.swW5Uk1icpIrXCN.WH6lC6uV5vwn6423iuArrPt9i6Jk1YG', ''),
+(2, 'test', '$2y$10$aZyY3.EsMrpu5WE2IHXFFuGRaEANYV7DL3cltu9065h7LjVkJFG4m', ''),
+(3, 'zonecaptain', '$2y$10$vLUSFZ8bwqHxBqpr.AiONOxyv8eSSaeKjOaq9s2Jmz88Mi740gp5W', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `config`
+--
+
+CREATE TABLE `config` (
+  `id` int(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `value_1` varchar(255) DEFAULT NULL,
+  `value_2` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`id`, `name`, `value_1`, `value_2`) VALUES
+(1, 'email', 'albertputrapurnama@gmail.com', NULL),
+(2, 'phone', '+1 925 238 6500', NULL),
+(3, 'ig', 'albertputrapurnama', NULL),
+(4, 'linkedin', 'albertputrapurnama', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experience`
+--
+
+CREATE TABLE `experience` (
+  `id` int(11) NOT NULL,
+  `year` varchar(255) NOT NULL,
+  `value` text NOT NULL,
+  `nested_value` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `experience`
+--
+
+INSERT INTO `experience` (`id`, `year`, `value`, `nested_value`) VALUES
+(1, '2012', 'Started Study At SMAK 5 BPK Penabur Jakarta', 'ayen kentang'),
+(2, '2012', 'CEO of Dazzle Corporation', 'Managed to import low budget sport equipment to middle-low class people. Developed deep relationship with forwarding companies responsible for importing goods from China. \n( 2012-Present )'),
+(3, '2013', 'test', NULL),
+(4, '2013', 'test2', 'asdawdawdasdawd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolio`
+--
+
+CREATE TABLE `portfolio` (
+  `id` int(11) NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+INSERT INTO `portfolio` (`id`, `uuid`, `name`, `link`) VALUES
+(1, '1', 'test', '1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quotes`
+--
+
+CREATE TABLE `quotes` (
+  `id` int(11) NOT NULL,
+  `value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quotes`
+--
+
+INSERT INTO `quotes` (`id`, `value`) VALUES
+(1, 'There has never yet been a man in our history who led a life of ease whose name is worth remembering - Theodore Roosevelt'),
+(2, 'Sometimes it is the people no one imagines anything of who do the things that no one can imagine - Alan Turing'),
+(3, 'Sometimes it is the people no one imagines anything of who do the things that no one can imagine - Alan Turing');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ability`
+--
+ALTER TABLE `ability`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `experience`
+--
+ALTER TABLE `experience`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `quotes`
+--
+ALTER TABLE `quotes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ability`
+--
+ALTER TABLE `ability`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `config`
+--
+ALTER TABLE `config`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `experience`
+--
+ALTER TABLE `experience`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `quotes`
+--
+ALTER TABLE `quotes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
